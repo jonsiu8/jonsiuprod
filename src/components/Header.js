bb045cc7b7b379528c21eae48dd2f81502923880
jon <jonsiu@gmail.com>
2019-02-01 01:05:10 +0800

    new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
-import logo from '../assets/images/logo.svg';
 import logo1 from '../assets/images/logo1.svg';
 
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo1} alt="" /></span>
-        <span className="logo"><img src={logo} alt="" /></span>
         {/*<h1>Jon Siu Productions</h1>*/}
     </header>
 )
